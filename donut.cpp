97e31faf98bb8d876974e1caa70496767173c976
hasan <hasankhonburhonov@gmail.com>
2025-03-24 13:59:36 +0500

    just a moving ball

---
+++ donut.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+const int WIDTH = 80;
+const int HEIGHT = 20;
+const float aspect = (float)WIDTH / HEIGHT;
+const float pixelAspect = 11.0f / 24.0f;
+
+char screen[HEIGHT][WIDTH];
+
+void gotoxy(int x, int y) {
+    std::cout << "\033[" << y << ";" << x << "H";
+}
+
+void print_screen() {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            std::cout << screen[i][j];
+        }
+        std::cout << '\n';
+    }
+    gotoxy(0, 0);
+    usleep(0.0005 * 1e6);
+}
+
+int main() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+
+    for (int t = 0;; t++) {
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                float x = (float)j / WIDTH * 2.0f - 1.0f;
+                float y = (float)i / HEIGHT * 2.0f - 1.0f;
+                
+                x *= aspect * pixelAspect;
+                x += sin(t * 0.001);
+
+                screen[i][j] = ' ';
+                if ((x * x + y * y) <= 0.5) {
+                    screen[i][j] = '@';
+                }
+            }
+        }
+    
+        print_screen();
+
+    }
+
+    return 0;
+}
